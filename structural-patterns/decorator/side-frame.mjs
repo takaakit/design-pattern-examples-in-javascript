5952cdddb5dd6acc2db529fdbb8970377d769b8d
takaakit <takaaki.teshima.dev@gmail.com>
2021-11-06 13:10:10 +0900

    Add input character check

--- structural-patterns/decorator/side-frame.mjs
+++ structural-patterns/decorator/side-frame.mjs
@@ -16,6 +16,11 @@ export class SideFrame extends Frame {
     constructor(display, frameChar) {
         // ˅
         super(display);
+
+        if (frameChar.length != 1) {
+            console.log('Only one character is allowed in a side frame.');
+            process.exit(1);
+        }
         this.frameChar = frameChar;
         // ˄
     }
