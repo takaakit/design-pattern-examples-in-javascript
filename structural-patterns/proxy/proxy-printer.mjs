b475bf41ff436c7dc7ebe8141a75a8ebef1fa1f5
takaakit <takaaki.teshima.dev@gmail.com>
2022-08-01 12:11:34 +0900

    Fix a comment typo

--- structural-patterns/proxy/proxy-printer.mjs
+++ structural-patterns/proxy/proxy-printer.mjs
@@ -48,7 +48,7 @@ export class ProxyPrinter extends Printer {
 
     output(content) {
         // ˅
-        // Check to see if the the RealPrinter had been created, create it if necessary.
+        // Check to see if the RealPrinter had been created, create it if necessary.
         if (this.real == null) {
             this.real = new RealPrinter(this.currentName);
         }
