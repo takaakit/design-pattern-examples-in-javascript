3511ef17eb0fa57d4e5d26a777bc47472d32a946
takaakit <takaaki.teshima.dev@gmail.com>
2022-08-11 12:12:34 +0900

    refactoring

--- behavioral-patterns/interpreter/context.mjs
+++ behavioral-patterns/interpreter/context.mjs
@@ -39,8 +39,8 @@ export class Context {
 
     slideToken(token) {
         // ˅
-		if (token !== this.tokens[this.currentIndex]) {
-			throw new Error('WARNING: ' + token + ' is expected but ' + this.tokens[this.currentIndex] + ' was found.');
+		if (token !== this.getToken()) {
+			throw new Error('WARNING: ' + token + ' is expected but ' + this.getToken() + ' was found.');
 		}
 		this.nextToken();
         // ˄
